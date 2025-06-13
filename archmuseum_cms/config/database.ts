e7b6d1bf6f54e6e655397de88e0288019dbd5b0e
Sargis <saqo1995s@gmail.com>
2025-06-13 11:33:27 -0700

    updated database.ts

--- archmuseum_cms/config/database.ts
+++ archmuseum_cms/config/database.ts
@@ -4,7 +4,7 @@
         connection: {
           host: env('DATABASE_HOST', 'localhost'),
           port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', ''),
+          database: env('DATABASE_NAME', 'archmuseum'),
           user: env('DATABASE_USERNAME', 'postgres'),
           password: env('DATABASE_PASSWORD', '0000'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not required
