b885f805efd6716cad25f5571c498516e630a33d
Sargis <saqo1995s@gmail.com>
2025-06-13 13:48:03 -0700

    fixing deployment issue

--- archmuseum_cms/config/database.ts
+++ archmuseum_cms/config/database.ts
@@ -2,14 +2,14 @@
       connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'archmuseum'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', '0000'),
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not required
           ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF'),
           },
         },
         debug: false,
